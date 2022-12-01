041795bc2856ee33db6893ead74f68a0078b5705
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2022-12-01 11:36:38 +0100

    Add files via upload

---
+++ Program_FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Largest number: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Write("Fizz: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Buzz: ");
+int b = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % a == 0 && i % b == 0)
+    {
+        Console.Write("FizzBuzz ");
+    }
+    else if (i % a == 0)
+    {
+        Console.Write("Fizz ");
+    }
+    else if (i % b == 0)
+    {
+        Console.Write("Buzz ");
+    }
+    else { Console.Write(i + " "); }
+}

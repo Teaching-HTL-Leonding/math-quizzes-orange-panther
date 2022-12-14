47aad3e750dacf0c7c35ee9039c2649a58dc6bdb
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2022-12-14 21:26:54 +0100

    Add files via upload

---
+++ Program_palindrome.cs
@@ -0,0 +1,25 @@
+﻿int numberOfDigits, palindrome = 0;
+
+Console.Write("Enter a positive integer number: ");
+int input = int.Parse(Console.ReadLine()!), inputCopy = input, inputCopy2 = input;
+
+for (numberOfDigits = 0; inputCopy > 0; numberOfDigits++)
+{
+    inputCopy = inputCopy / 10;
+}
+
+for (int counter= 0; counter != numberOfDigits; counter++)
+{
+    palindrome = palindrome * 10 + (input % 10);
+    input = (input - (input % 10)) / 10;
+}
+
+if (palindrome == inputCopy2)
+{
+    Console.WriteLine("This number is a palindrome.");
+}
+else 
+{
+    System.Console.WriteLine("This number is not a palindrome.");
+}
+
